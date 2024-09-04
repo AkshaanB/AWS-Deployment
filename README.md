0b4acbcdd2b536b521a25acd94d4503153f80e70
akshaan <akshaan@orelit.com>
2024-09-04 11:25:32 +0530

    Feat(Deployment) - Add initial implementation files.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AWS-Deployment
+
+Version - v0.0.0a01
+
 This repository contains a script to deploy ML/DL models to the AWS Sagemaker Endpoint
---
+++ deploy.py
@@ -0,0 +1,44 @@
+import argparse
+from typing import Union
+
+def delete_endpoint(region: str) -> str:
+    """Method to delete an endpoint
+
+    Args:
+        region (str): AWS region
+
+    Returns:
+        str: The response of endpoint deletion
+    """
+
+
+def create_endpoint(region: str, instance: str, model: str) -> Union[str, bool]:
+    """Method to create the endpoint
+
+    Args:
+        region (str): AWS region
+        instance (str): The instance of the endpoint
+        model (str): Model/s that need to be deployed
+
+    Returns:
+        Union[str, bool]: The response message of endpoint creation, True if successfully created False if not
+    """
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description="Deploying models to AWS Sagemaker endpoint")
+    
+    parser.add_argument('--delete', action='store_true', default=False, help='Delete the endpoint')
+    parser.add_argument('--create', action='store_true', default=False, help='Create the endpoint')
+    parser.add_argument('--region', default='us-east-1', type=str, help='Region that the model should be deployed')
+    parser.add_argument('--instance', default='m4.xlarge', type=str, help='The instance that the model is deployed')
+    parser.add_argument('--model', type=str, nargs='+', help='Model/s to be deployed')
+
+    args = parser.parse_args()
+
+    if args.delete:
+        response = delete_endpoint(args.region)
+        print(response)
+
+    if args.create:
+        response = create_endpoint(args.region, args.instance, args.model)
+        print(response)
